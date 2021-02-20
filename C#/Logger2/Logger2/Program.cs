2601aa10869e8a96d87dd9e61571bd4aed9d048b
Alexsander Vilaça <55555456+AlexsanderVilaca@users.noreply.github.com>
2021-02-20 00:16:29 -0300

    loggers

---
+++ C#/Logger2/Logger2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logger2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger a = new Logger();
+            a.SetConfigFile(@"C:\Documents and Settings\Administrator\My Documents\config.xml");
+            a.MessageBuilder("teste modbus", "teste");
+            Console.ReadKey();
+        }
+    }
+}
